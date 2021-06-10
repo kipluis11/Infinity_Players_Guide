f1045beb49ba8dfaa26c47ac341fecdca5f7caa4
kip_parcell <kipluis@gmail.com>
2021-06-10 11:29:10 -0700

    fixed user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :player_ratings
     has_many :units, through: :player_ratings
 
-    #validates :username, uniqueness: true, presence: true
+    validates :username, uniqueness: true, presence: true
 
     has_secure_password
 
