4eccab9ffea1f61d8c39840888a43e831627662c
kip_parcell <kipluis@gmail.com>
2021-06-10 09:31:02 -0700

    finished sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :player_ratings
-    has_many units, through: :player_ratings
+    has_many :units, through: :player_ratings
+
+    #validates :username, uniqueness: true, presence: true
+
+    has_secure_password
 
 end
