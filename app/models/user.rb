a16483ebd4cca04d7f8294a0915a9fbf8b3bee3d
kip_parcell <kipluis@gmail.com>
2021-06-10 17:18:23 -0700

    fixed Unit Has_Many relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :player_ratings
-    has_many :units, through: :player_ratings
+    has_many :rated_units, through: :player_ratings, source: :unit
+    has_many :units
 
     validates :username, uniqueness: true, presence: true
 
