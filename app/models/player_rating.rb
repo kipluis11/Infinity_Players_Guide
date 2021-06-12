c4dd3c2da841255354e4a2409bd163eb9e778cfa
kip_parcell <kipluis@gmail.com>
2021-06-12 13:10:13 -0700

    added scope validations

--- app/models/player_rating.rb
+++ app/models/player_rating.rb
@@ -4,4 +4,6 @@ class PlayerRating < ApplicationRecord
 
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true, greater_than_or equal_to: 1, less_than: 6}
+
+  validates :unit_id, uniqueness: {scope: :user, message: "already has a rating from you"}
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,9 +5,21 @@ class Unit < ApplicationRecord
   has_many :users, through: :player_ratings
   accepts_nested_attributes_for :faction
 
-  #def faction_attributes=(attributes)
-    #faction = Faction.find_or_create_by(
-      #attributes) if !name.empty?
+  validates :faction, presence: true
+  validates :name, uniqueness: {scope: :faction, message: "is already in the system and is ready to be rated"}
+
+  #this is for long validation
+  #validate :no_duplicate_faction
+
+  def faction_attributes=(name)
+    faction = Faction.find_or_create_by(
+      name) if !attributes['name'].empty?
+  end
+#this is long way to do validation, try scoping instead but keep this in case something breaks. 
+  #def no_duplicate_faction
+    #if Unit.find_by(name: name, faction_id: faction_id)
+     # errors. add(:name, "is already in the system and is ready to be rated")
+    #end
   #end
 
 end
